a0364040a6fa3725780ffb69d5a73c7326f0feca
bhavanakonchada <bhavanakonchada@users.noreply.github.com>
2020-05-17 23:03:12 -0700

    A problem in Hashing, and the other in Recursion (#1)
    Co-authored-by: Bhavana Konchada <bhkoncha@microsoft.com>

---
+++ Heap/MaxHeap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSProblems.Heap
+{
+    //TODO
+    class MaxHeap<T> : Heap<T>
+    {
+        public override bool Compare(T one, T two)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Recursion/NumberOfPathsInAMatrix.cs
@@ -0,0 +1,45 @@
+﻿using CSProblems.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+
+namespace CSProblems.Recursion
+{
+    class NumberOfPathsInAMatrix
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"The problem is to count all the possible paths from top left to bottom right of a " +
+                              $"MxN matrix with the constraints that from each cell you can either move to right or down.");
+
+           var inputMatrix = new string[3,3]
+            {
+                { "a","b","c" },{"d","e","f" },{"g","h","i" }
+            };
+
+            RecursivePath(inputMatrix, 0, 0, string.Empty);
+
+            Console.ReadLine();
+        }
+
+        private static void RecursivePath(string[,] inputMatrix, int row, int col, string result)
+        {
+           if(row == 2 && col == 2)
+            {                
+                Console.WriteLine(result + (inputMatrix[row, col]));
+                return;
+            }
+
+           if(row+1< 3)
+            {
+                RecursivePath(inputMatrix, row + 1, col, result+(inputMatrix[row,col]));
+            }
+
+           if(col+1 < 3)
+            {
+                RecursivePath(inputMatrix, row, col + 1, result+(inputMatrix[row, col]));
+            }
+        }
+    }
+}
