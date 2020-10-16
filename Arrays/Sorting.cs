59db3b078813f21d0c0de88859a0323f19248e03
bhavanakonchada <bhavanakonchada@users.noreply.github.com>
2020-10-15 17:08:48 -0700

    June2020 (#4)
    * A problem in Hashing, and the other in Recursion
    
    * more questions solved
    
    * topological sort.
    
    * corrected placement of file.
    
    * adding empty file for PR testing.
    
    * some more code
    
    Co-authored-by: Bhavana Konchada <bhkoncha@microsoft.com>

---
+++ Arrays/Sorting.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace CSProblems.Arrays
+{
+    class ArraySorting
+    {
+        public static void Main()
+        {
+            int[] input = new int[] { 23, 43, 5, 12, 89, 23, 18, 9, 38 };
+
+            Console.WriteLine($"Printing input array:");
+            //After the each iteration, the array looks like:
+            PrintArray(input);
+
+            //SortInNSquareTime(input);
+
+            SortInNLogNTime(input, 0, input.Length - 1);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintArray(int[] input)
+        {
+            for (int k = 0; k < input.Length; k++)
+            {
+                Console.Write(input[k] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void SortInNSquareTime(int[] input)
+        {
+            //Find the best possible way to sort this...
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        //swap them
+                        int temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp;
+                    }
+                }
+
+                Console.WriteLine($"Printing array after iteration {i}:");
+                //After the each iteration, the array looks like:
+                for (int k = 0; k < input.Length; k++)
+                {
+                    Console.Write(input[k] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[] SortInNLogNTime(int[] input, int start, int end)
+        {
+            if (start == end)
+            {
+                var output = new int[1];
+                output[0] = input[start];
+
+                return output;
+            }
+            int mid = (end + start) / 2;
+            return Merge(SortInNLogNTime(input, start, mid), SortInNLogNTime(input, mid + 1, end));
+        }
+
+        private static int[] Merge(int[] inputA, int[] inputB)
+        {
+            int a = 0;
+            int b = 0;
+            int i = 0;
+
+            Console.WriteLine("Merge called with inputs:");
+            PrintArray(inputA);
+            PrintArray(inputB);
+            Console.WriteLine();
+
+            int[] output = new int[inputA.Length + inputB.Length];
+
+            while (a < inputA.Length && b < inputB.Length)
+            {
+                if (inputA[a] <= inputB[b])
+                {
+                    output[i] = inputA[a];
+                    i++;
+                    a++;
+                }
+                else if (inputB[b] <= inputA[a])
+                {
+                    output[i] = inputB[b];
+                    b++;
+                    i++;
+                }
+            }
+
+            if (a == inputA.Length || b == inputB.Length) // items of one of the inputs are covered
+            {
+                //navigate thru all of remaining in inputB and include in the array
+                if (a < inputA.Length)
+                {
+                    while (a < inputA.Length)
+                    {
+                        output[i] = inputA[a];
+                        a++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    while (b < inputB.Length)
+                    {
+                        output[i] = inputB[b];
+                        b++;
+                        i++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Printing output after each merge");
+            PrintArray(output);
+            Console.WriteLine();
+
+            return output;
+        }
+    }
+}
+
+
+/*
+ *  int[] input = new int[] { 23, 43, 5, 12, 89, 23, 18, 9 };
+ *  Is there something called Binary sort ?
+ *  
+ *  Even number of items example
+ *  23,43,5,12    89,23,18,9
+ *  23,43,  5,12    89,23   18,9
+ *  23  43  5  12  89  23  18   9
+ *  23,43,  5,12  23,89   9,18
+ *  5,12,23,43     9,18,23,89   (starting from the left, compare each item and pick from each, to merge into a final list)
+ *  
+ *  5,9,12,18,23,23,43,89
+ *  
+ *  Odd Number example
+ *  
+ *  23,43,5,12,89,23,18,9,38
+ *  23,43,5,12,89 ,23,18,9,38
+ *  23,43,5    12,89   23,18   9,38
+ *  23,43  5   12   89  23  18  9  38
+ *  23  43  5   12   89  23  18  9  38
+ *  23,43   5   12,89    18,23   9,38
+ *  5,23,43     12,89     9, 18,23,38
+ *  5,12,23,43,89     9,18,23,38
+ *  
+ *  5,9,12,18,23,23,38,43,89
+ */
---
+++ Arrays/Sorting/InsertionSort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSProblems.Arrays.Sorting
+{
+    class InsertionSort
+    {
+        public static void Main()
+        {
+            var input = new int[] { 8, 2, 4, 9, 3, 6 };
+            Insertion_Sort(input);
+        }
+       public static void Insertion_Sort(int[] input)
+        {
+            var n = input.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = input[i];
+                int j = i - 1;
+                //while(j >= 0 && input[j] > key)
+                //{
+                //    //swap them
+                //    var temp = input[j];
+                //    input[j] = input[i];
+                //    input[i] = temp;
+
+                //   // key = input[j];
+                //    j--;
+                //}
+
+
+                while (j >= 0 && input[j] > key)
+                {
+                    input[j + 1] = input[j];
+                    j = j - 1;
+                }
+                input[j + 1] = key;
+
+
+                PrintArray(input);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public static int[] InsertionSortByShift(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                int j;
+                var insertionValue = inputArray[i];
+                for (j = i; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > insertionValue)
+                    {
+                        inputArray[j] = inputArray[j - 1];
+                    }
+                }
+                inputArray[j] = insertionValue;
+
+                PrintArray(inputArray);
+            }
+
+
+            return inputArray;
+        }
+
+        private static void PrintArray(int[] input)
+        {
+            for (int k = 0; k < input.Length; k++)
+            {
+                Console.Write(input[k] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ * Sample input: 8 2 4 9 3 6
+ * 
+ * 8 2 4 9 3 6
+ * 2 8 4 9 3 6
+ * 2 4 8 9 3 6
+ *          2 4 8 3 9 6
+ *          2 4 3 8 9 6
+ *          2 3 4 8 9 6
+ * 2 3 4 8 9 6
+ *         2 3 4 8 6 9
+ *         2 3 4 6 8 9
+ */
